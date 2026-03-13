acd295b8512a85654ebf4c81bb917504b8efaef4
Tri_Duong <sc15538@student.samk.fi>
2026-03-13 14:39:20 +0200

    created train_df file and val_df file

--- data_preparation.py
+++ data_preparation.py
@@ -4,13 +4,15 @@ import os
 images = sorted(os.listdir("data/images"))
 labels = sorted(os.listdir("data/labels"))
 
-data_df = pd.DataFrame(columns=["inputs", "outputs"])
+data_df = pd.DataFrame(columns=["image_path", "label_path"])
 
 data_df = pd.DataFrame({
-    "inputs": [f"data/images/{img}" for img in images],
-    "outputs": [f"data/labels/{lbl}" for lbl in labels]
+    "image_path": [f"data/images/{img}" for img in images],
+    "label_path": [f"data/labels/{lbl}" for lbl in labels]
 })
 
 data_df.to_csv("data/CSVs/dataset.csv", index=False)
+data_df.to_csv("data/CSVs/train_df.csv", index=False)
+data_df.to_csv("data/CSVs/val_df.csv", index=False)
 
 print(data_df)
