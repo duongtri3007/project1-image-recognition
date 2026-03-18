a94f61d63ec28118b314990224c82d6ea2fc29b5
Son_Nghiem <nghiemminhson_hsgs2021@hus.edu.vn>
2026-03-18 12:41:17 +0000

    add notes and change code's order

--- model.py
+++ model.py
@@ -5,13 +5,16 @@ from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 def build_model(backbone: str):
     if backbone == 'fasterrcnn_resnet50_fpn':
-        model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True, weights=weights)
         weights = torchvision.models.detection.FasterRCNN_ResNet50_FPN_Weights.DEFAULT
+        model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True, weights=weights)
 
     else:
-        model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=True, weights=weights)
         weights = torchvision.models.detection.FasterRCNN_MobileNet_V3_Large_FPN_Weights.DEFAULT
+        model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn(pretrained=True, weights=weights)
 
+    # in_feature = model.roi_heads.box_predictor.cls_score.in_features
+    # model.roi_heads.box_predictor = FastRCNNPredictor(in_feature, 2)
+    
     return model
 
 
